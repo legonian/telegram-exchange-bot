d49514a1be978515892bd29375a3c97fd7504078
Oleh <oleg.2014@gmail.com>
2021-03-07 15:45:32 +0200

    simplify plot code, add docs

--- plotter.py
+++ plotter.py
@@ -13,26 +13,34 @@ class Plotter:
     def __init__(self, max_time=10):
         self.lock = threading.Lock()
     
-    def _plot_by_dates(self, x, y, x_label, y_label):
-        plt.plot(x, y, 'k')
-        ax = plt.gca()
+    def _plot_by_dates(self, x, y, title):
+        fig, ax = plt.subplots()
+        ax.plot(x, y, 'k')
         ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
-        ax.grid(axis='y')
-        ax.set_xlabel(x_label)
-        ax.set_ylabel(y_label)
-        fig = plt.gcf()
+        ax.grid()
+        ax.set(title=title)
         fig.autofmt_xdate()
 
         return plt
 
     def plot_rates(self, x, y, rates_label):
+        """
+        Args:
+            param1 (obj): self
+            param2 (str): x axis values
+            param3 (str): y axis values
+            param4 (int): title of graph
+
+        Returns:
+            (bytes): image
+        """
         with self.lock:
-            plot = self._plot_by_dates(x, y, '', rates_label)
+            plot = self._plot_by_dates(x, y, rates_label)
 
             with io.BytesIO() as buf:
-                buf.truncate(0)
                 plot.savefig(buf)
                 plot.close()
+
                 buf.seek(0)
                 return buf.getvalue()
 
